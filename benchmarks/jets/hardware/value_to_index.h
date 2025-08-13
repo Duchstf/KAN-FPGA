12f8405f680a957dc0233ff4e6a877771c94ee7f
Duc Hoang <minhduc8199@gmail.com>
2025-08-13 07:23:21 -0400

    back up codes

--- benchmarks/jets/hardware/value_to_index.h
+++ benchmarks/jets/hardware/value_to_index.h
@@ -1,5 +1,5 @@
 int value_to_index(lut_input_t value) {
-    int data_round = (value * LUT_SIZE) >> {SHIFT_FACTOR};
+    int data_round = (value * LUT_SIZE) >> 4;
     int index = data_round + LUT_SIZE / 2;
 
     return index;
