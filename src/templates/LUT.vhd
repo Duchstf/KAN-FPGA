93496737902b8d1138a3b068a1e96b0a5faa7b5c
Duc Hoang <minhduc8199@gmail.com>
2025-08-21 12:14:34 -0400

    starting to support multiple layer quantization

--- src/templates/LUT.vhd
+++ src/templates/LUT.vhd
@@ -6,7 +6,7 @@ use work.PkgLUT.all;
 library xpm;
 use xpm.vcomponents.all;
 
-entity LUT is
+entity {{LUT_LAYER_NAME}} is
     generic (
         MEMFILE          : string ;
         READ_LATENCY     : integer  := 1;
@@ -14,32 +14,32 @@ entity LUT is
     );
     port (
         clk : in std_logic;
-        d : in lut_input_t;
-        q : out lut_output_t
+        d : in {{LUT_INPUT_TYPE}};
+        q : out {{LUT_OUTPUT_TYPE}}
     );
 end entity;
 
-architecture rtl of LUT is
+architecture rtl of {{LUT_LAYER_NAME}} is
 
-    signal idx : unsigned(LUT_ADDR_WIDTH-1 downto 0);
-    signal rom_q : std_logic_vector(LUT_DATA_WIDTH-1 downto 0);
+    signal idx : unsigned({{LUT_LAYER_NAME}}_ADDR_WIDTH-1 downto 0);
+    signal rom_q : std_logic_vector({{LUT_LAYER_NAME}}_DATA_WIDTH-1 downto 0);
 
     begin
 
         -- Convert signed to unsigned index 
-        idx <= unsigned(d + to_signed(2**(LUT_ADDR_WIDTH-1), LUT_ADDR_WIDTH));
+        idx <= unsigned(d + to_signed(2**({{LUT_LAYER_NAME}}_ADDR_WIDTH-1), {{LUT_LAYER_NAME}}_ADDR_WIDTH));
 
         -- Synchronous ROM read
         rom_i : xpm_memory_sprom
         generic map (
-            ADDR_WIDTH_A        => LUT_ADDR_WIDTH,
+            ADDR_WIDTH_A        => {{LUT_LAYER_NAME}}_ADDR_WIDTH,
             AUTO_SLEEP_TIME     => 0,
             MEMORY_INIT_FILE    => MEMFILE,
             MEMORY_INIT_PARAM   => "",
             MEMORY_OPTIMIZATION => "true",
             MEMORY_PRIMITIVE    => MEMORY_PRIMITIVE,
-            MEMORY_SIZE         => LUT_SIZE * LUT_DATA_WIDTH,
-            READ_DATA_WIDTH_A   => LUT_DATA_WIDTH,
+            MEMORY_SIZE         => {{LUT_LAYER_NAME}}_SIZE * {{LUT_LAYER_NAME}}_DATA_WIDTH,
+            READ_DATA_WIDTH_A   => {{LUT_LAYER_NAME}}_DATA_WIDTH,
             READ_LATENCY_A      => READ_LATENCY,
             READ_RESET_VALUE_A  => "0",
             USE_MEM_INIT        => 1
--- src/templates/PkgLUT.vhd
+++ src/templates/PkgLUT.vhd
@@ -3,11 +3,5 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 package PkgLUT is
-    constant LUT_SIZE : positive := {{LUT_SIZE}}; --LUT size
-    constant LUT_ADDR_WIDTH : positive := {{LUT_ADDR_WIDTH}}; --LUT address width = log2(LUT_SIZE)
-    constant LUT_DATA_WIDTH : positive := {{LUT_DATA_WIDTH}}; --LUT data width (=LUT address width in our case)
-    
-    subtype lut_output_t is signed(LUT_ADDR_WIDTH-1 downto 0);
-    subtype lut_input_t is signed(LUT_ADDR_WIDTH-1 downto 0);
-
+    {{LAYER_TYPES_BLOCK}}
 end package;
