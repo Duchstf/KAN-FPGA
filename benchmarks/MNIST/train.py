8cc7594b708ac8b69ffc27812fc471843101b053
Duc Hoang <minhduc8199@gmail.com>
2025-09-11 11:55:23 -0400

    back up codes

--- benchmarks/MNIST/train.py
+++ benchmarks/MNIST/train.py
@@ -48,7 +48,7 @@ config = {
     "learning_rate": 0.01,
     "weight_decay": 1e-4,
 
-    "prune_threshold": 0.05,
+    "prune_threshold": 0.8,
 }
 
 #Create a new directory to save the config and checkpoints
